580de5ac2740c47aa6d43c37e7459bc098ac51e9
L1ndseyHerman <lherman21@jcu.edu>
2021-10-05 09:57:58 -0400

    Caught up to vid309: storing the login token in localStorage so that the fake user will stay logged in if they refresh the page.

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -10,17 +10,23 @@ const AuthContext = React.createContext({
 
 export const AuthContextProvider = (props) => {
 
-    const [token, setToken] = useState(null);
+    const initialToken = localStorage.getItem('token');
+    //  Could be undefined or a string:
+    //  Is synchronous, so don't need a useEffect() for this.
+    const [token, setToken] = useState(initialToken);
 
     //  This somehow changes the type of the token to a boolean, false if empty string, true if smtng.
     const userIsLoggedIn = !!token;
 
     const loginHandler = (token) => {
         setToken(token);
+        //  local storage can only store primative data (no objects).
+        localStorage.setItem('token', token);
     };
 
     const logoutHandler = () => {
         setToken(null);
+        localStorage.removeItem('token');
     };
 
     const contextValue = {
